d45157d330e7e41b604e10e7ba5c0c1d1b589441
MXW9699 <mitchell.wen@hotmail.com>
2023-11-07 12:31:33 -0500

    set up webpack and environment

---
+++ webpack.config.js
@@ -0,0 +1,58 @@
+const path = require('path');
+const HTMLwebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  //switch between production and development
+  mode: process.env.NODE_ENV,
+  //entry point for loading all the files
+  entry: ['./client/index.js'],
+
+  //location to create the bundle.js file that will contain the consolidated application
+  output: [
+    {
+      //location will be the build folder created in the root
+      path: path.join(__dirname, 'build'),
+      //bundle.js will be the name
+      file: 'bundle.js',
+    },
+  ],
+  //setup development server
+  devServer: {
+    //listen on port 8080
+    port: 8080,
+    static: { directory: path.join(__dirname, 'build'), publicPath: '/' },
+    //set up proxies
+    proxy: {},
+  },
+
+  //template to follow when creating the webserver for dev enviroment
+  plugins: [new HTMLwebpackPlugin({ template: 'index.html' })],
+
+  module: {
+    //module rules for transpiling and compliling
+    rules: [
+      //rules to transpile js and jsx files
+      {
+        test: /.jsx?/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: [
+              //changes es6 format of js files to es5
+              ['@babel/preset-env', { targets: 'defaults' }],
+              //transpile jsx to js
+              ['@babel/preset-react', { targets: 'defaults' }],
+            ],
+          },
+        },
+      },
+      //rules to change sass files to css to strings that will be injected into the html body
+      {
+        test: /\.s?css/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader'],
+      },
+    ],
+  },
+};
