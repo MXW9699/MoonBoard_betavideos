eda0b82c89cbbb55503afdb68942239849f88a4b
MXW9699 <mitchell.wen@hotmail.com>
2024-04-06 23:57:15 -0400

    slidable with numbers and snapping

--- components/forms/filterPage.jsx
+++ components/forms/filterPage.jsx
@@ -1,15 +1,26 @@
-import React, { useRef, useEffect } from 'react';
+import React, { useRef, useEffect, useState } from 'react';
 
 //some kind of scale
+//bar.width
+//divide into sections bar.width/range
+//
+
 export default function FilterPage() {
+  const [current, setCurrent] = useState(0);
   const slide = useRef(null);
+  const notch = useRef(null);
+  const SEGMENTS = 15;
 
   function draggable(e) {
     e.preventDefault();
     let start = e.clientX - parseInt(e.target.style.left || 0, 10);
     const mouseMoveHandler = (mouse) => {
-      let delta = mouse.clientX - start;
+      let width = slide.current.clientWidth;
+      let delta = Math.min(Math.max(mouse.clientX - start, 0), width);
+      let snap = Math.floor((delta / width) * SEGMENTS);
       e.target.style.left = `${delta}px`;
+
+      setCurrent(snap);
     };
     window.addEventListener('mousemove', mouseMoveHandler);
     window.addEventListener('mouseup', () => {
@@ -17,22 +28,41 @@ export default function FilterPage() {
     });
   }
 
+  function readjust() {
+    notch.current.style.left = `${
+      (current * slide.current.clientWidth) / SEGMENTS
+    }px`;
+  }
+
+  useEffect(() => {
+    window.addEventListener('mouseup', readjust);
+    return () => {
+      window.removeEventListener('mouseup', readjust);
+    };
+  }, [current]);
+
   return (
-    <div style={{ backgroundColor: 'green', width: '100%', height: '10px' }}>
+    <>
+      {current}
       <div
-        style={{ backgroundColor: 'blue', position: 'absolute' }}
+        style={{ backgroundColor: 'green', width: '100%', height: '10px' }}
         ref={slide}
-        onMouseDown={draggable}
       >
-        O
-      </div>
-      <div
+        <div
+          style={{ backgroundColor: 'blue', position: 'absolute' }}
+          onMouseDown={draggable}
+          ref={notch}
+        >
+          O
+        </div>
+        {/* <div
         style={{ backgroundColor: 'yellow', position: 'absolute' }}
         ref={slide}
         onMouseDown={draggable}
       >
         O
+      </div> */}
       </div>
-    </div>
+    </>
   );
 }
